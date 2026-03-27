802ca35a4d014b83ad63fd08d8033ed891e6c54c
Marvin Atorf <atorfmarvin@gmail.com>
2026-03-27 12:02:35 +0100

    Update `library_db.sql` to enhance schema with timestamps and foreign key constraints

--- db/library_db.sql
+++ db/library_db.sql
@@ -4,11 +4,15 @@ CREATE TABLE authors (
     author_id   INT PRIMARY KEY AUTO_INCREMENT,
     first_name  VARCHAR(100) NOT NULL,
     last_name   VARCHAR(100) NOT NULL,
-    rating      VARCHAR(5) DEFAULT 1
+    rating      VARCHAR(5) DEFAULT 1,
+    created_at      TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE books (
-    book_id INT PRIMARY KEY AUTO_INCREMENT,
-    title varchar(50) NOT NULL,
-
-)
+    book_id         INT PRIMARY KEY AUTO_INCREMENT,
+    author_id       INT NOT NULL,
+    title           VARCHAR(255) NOT NULL,
+    published_at    DATE NOT NULL,
+    created_at      TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (author_id) REFERENCES authors(author_id)
+);
